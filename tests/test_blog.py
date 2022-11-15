488b43da26e25ab0c5834c69d022d1265a83a3a9
JP Fernandes <joaopedro.jpbf@gmail.com>
2022-11-15 17:47:12 -0300

    Testes adicionados

---
+++ tests/test_blog.py
@@ -0,0 +1,17 @@
+import pytest
+from flaskr.db import get_db
+
+
+def test_index(client, auth):
+    response = client.get('/')
+    assert b"Log in" in response.data
+    assert b"Register" in response.data
+
+    auth.login()
+    response = client.get('/')
+    assert b'Log Out' in response.data
+    assert b'test title' in response.data
+    assert b'by test on 2018-01-01' in response.data
+    assert b'test\nbody' in response.data
+    assert b'href="/1/update"' in response.data
+
